13413b4ca12c1ea36624fca0025f3fb58341ad23
P-Ramos16 <p.ramos@ua.pt>
2024-05-16 23:03:15 +0100

    Rewrote some of the signage

--- scripts/script.js
+++ scripts/script.js
@@ -1,45 +1,53 @@
 //  Do on window load
 window.onload = function() {
-    updateClock();
-    setInterval(updateClock, 10000);
-    getNums();
-    setInterval(getNums, 10000);
-    getNextNums();
-    setInterval(getNextNums, 10000);
+  updateClock();
+  setInterval(updateClock, 10000);
+  getNums();
+  setInterval(getNums, 10000);
+  getNextNums();
+  setInterval(getNextNums, 10000);
 }
 
 //  Update the clock every 10 seconds
 function updateClock() {
-    var date = new Date();
+  var date = new Date();
 
-    var hour = (date.getHours() < 10 ? '0' : '') + date.getHours();
-    var minutes = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes()
+  var hour = (date.getHours() < 10 ? '0' : '') + date.getHours();
+  var minutes = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes()
 
-    document.getElementById("clock").innerHTML = hour + ":" + minutes;
+  document.getElementById("clock").innerHTML = hour + ":" + minutes;
 }
 
 function getNums() {
-    fetch("http://localhost:8090/signage/getNums").then(function(response) {
+  fetch("http://localhost:8080/signage", {method: "POST"}).then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      fetch("http://localhost:8080/signage").then(function(response) {
         return response.json();
-      })
-      .then(function(data) {
-        fetch("http://localhost:8090/signage/nextNum").then(function(response) {
-            return response.json();
-          })
-          .then(function(data) {
-            document.getElementById("nextNumber").innerHTML = data["num"];
-          })
-          .catch(function(err) {
-            console.log('Fetch Error :-S', err);
-          });
-        for (const key in data) {
-            document.getElementById("num" + key).innerHTML = data[key][0];
-            document.getElementById("kiosk" + key).innerHTML = data[key][1];
-        }
-      })
-      .catch(function(err) {
-        console.log('Fetch Error :-S', err);
-      });
+        })
+        .then(function(data) {
+          console.log(data);
+
+          for (const key in data) {
+            console.log("setting for key " + key + " and data " + data[key])
+            if(key == "nextnum") {
+              document.getElementById("nextNumber").innerHTML = data[key];
+            }
+            else {
+              document.getElementById("num" + key).innerHTML = data[key][0];
+              document.getElementById("kiosk" + key).innerHTML = data[key][1];
+            }
+          }
+        })
+        .catch(function(err) {
+          console.log('Fetch Error :-S', err);
+        });
+    })
+    .catch(function(err) {
+      console.log('Fetch Error :-S', err);
+    });
 }
 
 function getNextNums() {
